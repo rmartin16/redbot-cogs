17443ab820db605042176f9f9b341fcab0dc660c
Russell Martin <rmartin16@gmail.com>
2022-06-22 13:16:34 -0400

    Ensure images are properly embedded...attempt 5

--- dalle/dalle.py
+++ dalle/dalle.py
@@ -133,7 +133,7 @@ class DallE(commands.Cog):
             try:
                 await ctx.guild._state.http.request(r, form=form, files=files_images_chunk.values())
             except discord.errors.DiscordServerError as e:
-                return await ctx.send(f"Discord is sucking... >:( {e}"
+                await ctx.send(f"Discord is sucking... >:( {e}")
 
     @staticmethod
     async def generate_images(prompt: str, num_of_images: int = 1) -> Union[List[io.BytesIO], int, str]:
