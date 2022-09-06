08d826f4b69afd993883a19ae0e672db2547a061
Russell Martin <rmartin16@gmail.com>
2022-09-05 23:36:17 -0400

    Stable diffusion updates

--- stablediffusion/stablediffusion.py
+++ stablediffusion/stablediffusion.py
@@ -168,9 +168,10 @@ class StableDiffusion(commands.Cog):
                                 images.append(io.BytesIO(await image.content.read()))
                         else:
                             current_step += 1
-                            if current_step == total_steps or current_step % 5:
+                            if current_step == total_steps or current_step % 5 == 0:
                                 await interim_msg.edit(content=msg_template.format(current_step))
 
+                await interim_msg.delete()
                 return images
         except aiohttp.ClientConnectionError as e:
             return f"Stable Diffusion backend is probably down [{e}]"
