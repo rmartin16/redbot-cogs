8ca626beee17dcba6b229fa13c19e2d53ff6adc4
Russell Martin <rmartin16@gmail.com>
2022-12-14 16:00:14 -0500

    update

--- chatgpt/__init__.py
+++ chatgpt/__init__.py
@@ -4,6 +4,5 @@ from .chatgpt import ChatGPT
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 
-async def setup(bot):
-    n = ChatGPT(bot)
-    bot.add_cog(n)
+def setup(bot):
+    bot.add_cog(ChatGPT(bot))
