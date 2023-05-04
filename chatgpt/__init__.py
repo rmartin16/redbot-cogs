f6488ff94c45aff4efd36233e8122d4cce374106
Russell Martin <rmartin16@gmail.com>
2023-05-04 10:50:10 -0400

    Actually await `setup()`

--- chatgpt/__init__.py
+++ chatgpt/__init__.py
@@ -4,5 +4,5 @@ from .chatgpt import ChatGPT
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 
-def setup(bot):
+async def setup(bot):
     await bot.add_cog(ChatGPT(bot))
