bd86d4f8551ec1734a4a57a29a53ee03ce612163
Russell Martin <rmartin16@gmail.com>
2023-05-04 10:34:27 -0400

    Remove `setup()` since its in `__init__.py`

--- stablediffusion/stablediffusion.py
+++ stablediffusion/stablediffusion.py
@@ -294,7 +294,3 @@ class StableDiffusion(commands.Cog):
 
         async with aiohttp.ClientSession() as session:
             await session.get(f"{STABLEDIFFUSION_POST_ENDPOINT}/cancel")
-
-
-async def setup(bot):
-    await bot.add_cog(StableDiffusion(bot))
