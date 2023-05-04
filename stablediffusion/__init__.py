c2158e017a6c1b52b4c330eca08237d1a346bebd
Russell Martin <rmartin16@gmail.com>
2023-05-04 10:35:05 -0400

    Await `setup()` with redbot 3.5

--- stablediffusion/__init__.py
+++ stablediffusion/__init__.py
@@ -5,5 +5,4 @@ __red_end_user_data_statement__ = "This cog does not persistently store data or 
 
 
 async def setup(bot):
-    n = StableDiffusion(bot)
-    bot.add_cog(n)
+    await bot.add_cog(StableDiffusion(bot))
